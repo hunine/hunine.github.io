7d0ade784c9002f229a214c83bc0363672628dc5
H2Q318 <hqhung201@gmail.com>
2022-06-04 17:22:03 +0700

    Test :tada:

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var img = document.getElementById("popcat1");
 var count = document.getElementById("score");
 var score = 0;
-var MyScore = 10000;
 var audio = new Audio('pop.mp3');
 var hk_score = 1000, tw_score = 430, th_score = 200, jp_score = 150, fi_score = 250, se_score = 100, pl_score = 500, dm_score = 280, id_score=590, hu_score=319, sr_score = 300; 
 
@@ -31,6 +30,5 @@ img.addEventListener("touchmove", function(){
 
 function increaseScore(){
     score++;
-    MyScore++;
     count.innerHTML = score;
 }
