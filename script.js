c4a3f22e6efa597ccb51c535f96a58f9d249b299
H2Q318 <hqhung201@gmail.com>
2022-06-04 17:08:52 +0700

    LOL

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var img = document.getElementById("popcat1");
 var count = document.getElementById("score");
-var malaysiaScore = document.getElementById('my_score');
 var score = 0;
 var MyScore = 10000;
 var audio = new Audio('pop.mp3');
@@ -30,89 +29,8 @@ img.addEventListener("touchmove", function(){
      audio.play();
 });
 
-
-// Score on leaderboard
-setInterval(startCountHk, 1);
-
-function startCountHk(){
-    hk_score++;
-    document.getElementById("table").rows[1].cells.item(3).innerHTML = hk_score;
-}
-
-setInterval(startCountTw, 20);
-
-function startCountTw(){
-    tw_score++;
-    document.getElementById("table").rows[2].cells.item(3).innerHTML = tw_score;
-}
-
-setInterval(startCountTh, 25);
-
-function startCountTh(){
-    th_score++;
-    document.getElementById("table").rows[3].cells.item(3).innerHTML = th_score;
-}
-
-setInterval(startCountJp, 40);
-
-function startCountJp(){
-    jp_score++;
-    document.getElementById("table").rows[4].cells.item(3).innerHTML = jp_score;
-}
-
-setInterval(startCountFi, 34);
-
-function startCountFi(){
-    fi_score++;
-    document.getElementById("table").rows[6].cells.item(3).innerHTML = fi_score;
-}
-
-setInterval(startCountSe, 20);
-
-function startCountSe(){
-    se_score++;
-    document.getElementById("table").rows[7].cells.item(3).innerHTML = se_score;
-}
-
-setInterval(startCountPl, 15);
-
-function startCountPl(){
-    pl_score++;
-    document.getElementById("table").rows[8].cells.item(3).innerHTML = pl_score;
-}
-
-setInterval(startCountDm, 31);
-
-function startCountDm(){
-    dm_score++;
-    document.getElementById("table").rows[9].cells.item(3).innerHTML = dm_score;
-}
-
-setInterval(startCountId, 29);
-
-function startCountId(){
-    id_score++;
-    document.getElementById("table").rows[10].cells.item(3).innerHTML = id_score;
-}
-
-setInterval(startCountHu, 70);
-
-function startCountHu(){
-    hu_score++;
-    document.getElementById("table").rows[11].cells.item(3).innerHTML = hu_score;
-}
-
-setInterval(startCountSr, 5);
-
-function startCountSr(){
-    sr_score++;
-    document.getElementById("table").rows[12].cells.item(3).innerHTML = sr_score;
-}
-
 function increaseScore(){
     score++;
     MyScore++;
     count.innerHTML = score;
-    malaysiaScore.innerHTML = MyScore;
-    document.getElementById("table").rows[5].cells.item(3).innerHTML = MyScore;
 }
